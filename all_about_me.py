9f49e7e57eef678776d8c38f8d0570faaa37bb97
Jafer <kumikaraka@gmail.com>
2026-09-03 20:41:47 +0300

    first python program

---
+++ all_about_me.py
@@ -0,0 +1,10 @@
+# This program introduces me using variables.
+name = "Kumi Yusif"
+age = 23
+favourite_language = "Python"
+goal = "build useful programs"
+
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"My favourite programming language is {favourite_language}.")
+print(f"My goal for this course is to {goal}.")
